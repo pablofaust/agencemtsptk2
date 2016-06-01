fad8e5a0860dad865c5e27d9456dbfa5a605a5e0
pabfaust <pabfaust@gmail.com>
2016-06-01 16:15:57 +0200

    oegirjoerij

--- home/index.php
+++ home/index.php
@@ -3,7 +3,6 @@
 <?php include $route.'/assets/header.php' ?>
 <body>
 
-<?php include $route.'/home/background.php' ?>
 <?php include $route.'/home/front.php' ?>
 <?php include $route.'/home/home-footer.php' ?>
 
